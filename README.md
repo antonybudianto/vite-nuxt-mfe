f438d49f55af1a37daaab373823f88dc2ee95252
antony <antonybudianto@gmail.com>
2024-12-20 22:58:41 +0700

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# MFE nuxt x nuxt
+
+MFE with Nuxt being the Host and Remote 
+
+```
+cd host
+pnpm i
+pnpm generate
+pnpm serve
+```
+
+```
+cd remote
+pnpm i
+pnpm generate
+pnpm serve
+```
+
+# TODO
+
+- Enable CORS on remote JS assets
+- Scoped style still not works
---
+++ remote/nuxt.config.ts
@@ -0,0 +1,24 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import federation from '@originjs/vite-plugin-federation'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: false },
+  ssr: false,
+  vite: {
+    plugins: [
+      federation({
+        name: 'remote-app',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './RemoteComponent': './components/RemoteComponent.vue'
+        },
+        shared: []
+        // shared: ['vue']
+      })
+    ],
+    build: {
+      target: 'esnext'
+    }
+  }
+})
